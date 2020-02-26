197cb2b540889bb59af4c4cf70ea34d0b7a9cb55
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2020-02-26 19:15:24 +0100

    working on 2 arduinos

--- Wasserstand_master/Wasserstand_master.ino
+++ Wasserstand_master/Wasserstand_master.ino
@@ -38,10 +38,7 @@ void loop() { //////////////////////////////////////////////////////////////////
     lastMeasure = millis();
 
     String measured = measure();
-//    if(isDigit(measured.charAt(0))){
-//      Serial.println("##########VALID");
-//      }
-    measured = measured.substring(11,measured.length()-3);
+    
 
 
     Serial.println(measured);
@@ -66,16 +63,13 @@ String measure() {  ////////////////////////////////////////////////////////////
   String waterStr = "";
   Serial.println("howMuchWater?");
 
-  //Serial.println("whaiting for line...");
   String line = Serial.readString();
 
   long start = millis();
 
   while (line == "") {
-    //Serial.println("in while");
     int tmp = millis() - start;
     if (tmp >= timeout) {
-      //Serial.println("timeout");
       line = "timeout";
 
       //      if(currentTimeouts >= maxTimeouts){
@@ -92,33 +86,22 @@ String measure() {  ////////////////////////////////////////////////////////////
       break;
     }
 
-//    if (line.indexOf("waterLevel:") > 0) {
-//      Serial.println("found smth");
-//      waterStr = line;
-//      break;
 //    }
 
 
-    //Serial.println("readNewString...");
     line = Serial.readString();
   }
 
-  //Serial.println("out of while");
 
-  //Serial.println("Antwort: " + waterStr + "antwort ENDE");
-  //if(isDigit(line){
 
   //}
+  line = line.substring(11,line.length()-3);
 
 
-  if(line.indexOf("waterLevel:") > 0){
-    line = getValue(line, ':', 1);
-    }
 
-
-  //int waterLevel_bigTank = ;
-  //int waterLevel_smallTank = getWaterLevel();
-  //int waterLevel = waterLevel_bigTank + waterLevel_smallTank;
+  int waterLevel_bigTank = line.toInt();
+  int waterLevel_smallTank = getWaterLevel();
+  int waterLevel = waterLevel_bigTank + waterLevel_smallTank;
 
 
 
@@ -126,177 +109,92 @@ String measure() {  ////////////////////////////////////////////////////////////
   //String waterLevel_str = String(waterLevel_bigTank) + ";" + String(waterLevel_smallTank) + String(waterLevel);
   //String waterLevel_str = String(waterLevel_bigTank);
   //Serial.println(waterLevel_str);
-
   
   
-  return line;
+  
+  return String(waterLevel);
 }
 
 
 
-//int getWaterLevel(){////////////////////////////////////////////////////////////////////////////////////////////7/////////////////////////////////////////////////////////////////////////////////GET WATER LEVEL
-//  int currentWaterLevel = 0;
-//
-//  for(int i = 0; i <= 15; i++){
-//    if(getValueForSensor(i) == 0){
-//        currentWaterLevel += waterBetweenSensors;
-//      }
-//    else{
-//      break;
-//      }
-//    }
-//
-//
-//  if(getValueForSensor(14) == 0 && getValueForSensor(15) == 0){
-//      currentWaterLevel = 8000;
-//  }
-//
-//  return currentWaterLevel;
-//
-//
-//  }
-
-
-
-
-//int getValueForSensor(int sensorNum){/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////GET VALUE FOR SENSOR
-//
-//  switch(sensorNum){
-//    case 0:
-//      return analogRead(A0);
-//      break;
-//    case 1:
-//      return analogRead(A1);
-//      break;
-//    case 2:
-//      return analogRead(A2);
-//      break;
-//    case 3:
-//      return analogRead(A3);
-//      break;
-//    case 4:
-//      return analogRead(A4);
-//      break;
-//    case 5:
-//      return analogRead(A5);
-//      break;
-//    case 6:
-//      return analogRead(A6);
-//      break;
-//    case 7:
-//      return analogRead(A7);
-//      break;
-////    case 8:
-////      return analogRead(A8);
-////      break;
-////    case 9:
-////      return analogRead(A9);
-////      break;
-////    case 10:
-////      return analogRead(A10);
-////      break;
-////    case 11:
-////      return analogRead(A11);
-////      break;
-////    case 12:
-////      return analogRead(A12);
-////      break;
-////    case 13:
-////      return analogRead(A13);
-////      break;
-////    case 14:
-////      return analogRead(A14);
-////      break;
-////    case 15:
-////      return analogRead(A15);
-////      break;
-//
-//    }
-//    return 100;
-//
-//  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
+int getWaterLevel(){////////////////////////////////////////////////////////////////////////////////////////////7/////////////////////////////////////////////////////////////////////////////////GET WATER LEVEL
+  int currentWaterLevel = 0;
+
+  for(int i = 0; i <= 7; i++){
+    if(getValueForSensor(i) == 0){
+        currentWaterLevel += waterBetweenSensors;
+      }
+    else{
+      break;
+      }
     }
+
+
+  if(getValueForSensor(6) == 0 && getValueForSensor(7) == 0){
+      currentWaterLevel = 4000;
   }
 
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
+  return currentWaterLevel;
+
+
+  }
+
+
+
+
+int getValueForSensor(int sensorNum){/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////GET VALUE FOR SENSOR
+
+  switch(sensorNum){
+    case 0:
+      return analogRead(A0);
+      break;
+    case 1:
+      return analogRead(A1);
+      break;
+    case 2:
+      return analogRead(A2);
+      break;
+    case 3:
+      return analogRead(A3);
+      break;
+    case 4:
+      return analogRead(A4);
+      break;
+    case 5:
+      return analogRead(A5);
+      break;
+    case 6:
+      return analogRead(A6);
+      break;
+    case 7:
+      return analogRead(A7);
+      break;
+//    case 8:
+//      return analogRead(A8);
+//      break;
+//    case 9:
+//      return analogRead(A9);
+//      break;
+//    case 10:
+//      return analogRead(A10);
+//      break;
+//    case 11:
+//      return analogRead(A11);
+//      break;
+//    case 12:
+//      return analogRead(A12);
+//      break;
+//    case 13:
+//      return analogRead(A13);
+//      break;
+//    case 14:
+//      return analogRead(A14);
+//      break;
+//    case 15:
+//      return analogRead(A15);
+//      break;
+
+    }
+    return 100;
+
+  }
